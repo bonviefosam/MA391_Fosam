478676d9c3bf1d7243540dc07fa17786c49dede4
bonviefosam <60330882+bonviefosam@users.noreply.github.com>
2020-04-02 20:25:18 -0400

    Add files via upload

---
+++ Block_Two_WorksCited.r
@@ -0,0 +1,6 @@
+###############Works Cited#########
+#Meerschaert, Mark. Solutions Manual for Mathematical Modeling. Chapter 4. Accessed April 2, 2020.
+###I used this source to help me figure out how to integrate the given information into the objective 
+###function of problem 10 of chapter 4. After trying to formulate the equation on my own and struggling
+###I referenced this source to understand the proper way to structure the function and apply the given 
+###from the problem. After understanding this I was able to complete the rest of the problem on my own.
